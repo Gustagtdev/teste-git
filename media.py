27f3238e3beeb5c6c6fccb44051158946e354d60
Gustavo Rodrigues Lopes Lopes <gustavorodlopes03@gmail.com>
2024-12-23 12:29:14 -0300

    projeto inicial

---
+++ media.py
@@ -0,0 +1,42 @@
+'''Faça um programa que realize a média de 2 notas de 3 estudantes e faça os requisitos de aprovação,
+recuperação e reprovação.
+Este projeto possui o intuito em aprimorar minhas habilidades com a ferramenta Git.'''
+aluno1= "Igor"
+aluno2= "João"
+aluno3= "Rafaela"
+nota1_aluno1= float(input(f'Digite a 1° nota de {aluno1}:'))
+nota2_aluno1= float(input(f'Digite a 2° nota de {aluno1}: '))
+media= (nota1_aluno1+nota2_aluno1) / (2)
+print(media)
+
+nota1_aluno2= float(input(f'Digite a 1° nota de {aluno2}:'))
+nota2_aluno2= float(input(f'Digite a 2° nota de {aluno2}: '))
+media2= (nota1_aluno2+nota2_aluno2) / (2)
+print(media2)
+
+nota1_aluno3= float(input(f'Digite a 1° nota de {aluno3}:'))
+nota2_aluno3= float(input(f'Digite a 2° nota de {aluno3}: '))
+media3= (nota1_aluno3+nota2_aluno3) / (2)
+print(media3)
+
+if media >= 6.0:
+    print(f"{aluno1} está aprovado")
+elif 6.0 > media > 4.0:
+    print(f"{aluno1} está de recuperação")
+elif media < 4.0:
+    print(f"{aluno1} está reprovado")
+
+if media2 >= 6.0:
+    print(f"{aluno2} está aprovado")
+elif 6.0 > media2 > 4.0:
+    print(f"{aluno2} está de recuperação")
+elif media2 < 4.0:
+    print(f"{aluno2} está reprovado")
+
+if media3 >= 6.0:
+    print(f"{aluno3} está aprovado")
+elif 6.0 > media3 > 4.0:
+    print(f"{aluno3} está de recuperação")
+elif media < 4.0:
+    print(f"{aluno3} está reprovado")
+
