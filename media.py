fd54874e799c15548280583a503b717b1e735ad3
Gustavo Rodrigues Lopes Lopes <gustavorodlopes03@gmail.com>
2024-12-23 16:03:41 -0300

    adicionar a média de mais um aluno

--- media.py
+++ media.py
@@ -4,6 +4,7 @@ Este projeto possui o intuito em aprimorar minhas habilidades com a ferramenta G
 aluno1= "Igor"
 aluno2= "João"
 aluno3= "Rafaela"
+aluno4="Gustavo"
 nota1_aluno1= float(input(f'Digite a 1° nota de {aluno1}:'))
 nota2_aluno1= float(input(f'Digite a 2° nota de {aluno1}: '))
 media= (nota1_aluno1+nota2_aluno1) / (2)
@@ -18,6 +19,10 @@ nota1_aluno3= float(input(f'Digite a 1° nota de {aluno3}:'))
 nota2_aluno3= float(input(f'Digite a 2° nota de {aluno3}: '))
 media3= (nota1_aluno3+nota2_aluno3) / (2)
 print(media3)
+nota1_aluno4= float(input(f'Digite a 1° nota de {aluno4}:'))
+nota2_aluno4= float(input(f'Digite a 2° nota de {aluno4}: '))
+media4= (nota1_aluno4+nota2_aluno4) / (2)
+print(media4)
 
 if media >= 6.0:
     print(f"{aluno1} está aprovado")
@@ -40,3 +45,9 @@ elif 6.0 > media3 > 4.0:
 elif media < 4.0:
     print(f"{aluno3} está reprovado")
 
+if media4 >= 6.0:
+    print(f"{aluno3} está aprovado")
+elif 6.0 > media4 > 4.0:
+    print(f"{aluno4} está de recuperação")
+elif media < 4.0:
+    print(f"{aluno4} está reprovado")
